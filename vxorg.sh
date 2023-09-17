cd8845be3d8a9f2d756b473628eebd7492049be8
Nelson Chen <crazysim@gmail.com>
2023-09-16 21:50:16 -0700

    oh hey this works

--- vxorg.sh
+++ vxorg.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
-GPU_BOARD=$(nvidia-xconfig --query-gpu-info | grep GPU | awk '{print $4}')
+GPU_BOARD=$(nvidia-xconfig --query-gpu-info | grep -i 'Name' | awk -F ': ' '{print $2}')
 GPU_PCI=$(nvidia-xconfig --query-gpu-info | grep BusID | awk '{print $4}')
-# GPU_PCI="PCI:9:0:0"
 
 mkdir -p /etc/X11
 cat <<EOT > /etc/X11/xorg.conf
 Section "Files"
-    ModulePath      "/usr/lib/xorg/modules"
-    ModulePath      "/usr/local/nvidia"
+		ModulePath      "/usr/lib/xorg/modules"
+		ModulePath      "/usr/local/nvidia"
 EndSection
 
 Section "Device"
-    Identifier     "Device0"
-    Driver         "nvidia"
-    VendorName     "NVIDIA Corporation"
-    BoardName      "$GPU_BOARD"
-    BusID          "$GPU_PCI"
+		Identifier     "Device0"
+		Driver         "nvidia"
+		VendorName     "NVIDIA Corporation"
+		BoardName      "$GPU_BOARD"
+		BusID          "$GPU_PCI"
+EndSection
+
+Section "Monitor"
+    Identifier     "Monitor0"
+    VendorName     "Unknown"
+    ModelName      "Unknown"
+    Option         "DPMS"
+EndSection
+
+Section "Screen"
+    Identifier     "Screen0"
+    Device         "Device0"
+    Monitor        "Monitor0"
+    DefaultDepth    24
+    SubSection     "Display"
+        Virtual     1920 1080
+        Depth       24
+				Modes       "1920x1080"
+    EndSubSection
 EndSection
 EOT
 
-# Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./xserver.log vt1 $DISPLAY &
+Xorg -noreset +extension GLX +extension RANDR +extension RENDER -logfile ./shared/xserver.log vt1 $DISPLAY &
